ac215ebdf5f23c3cb5127cb5be8a65e1d15ebf08
vipeachhh <vipeachhh@gmail.com>
2023-02-27 00:51:26 +0500

    Update second task

--- task2.js
+++ task2.js
@@ -17,55 +17,109 @@ const data = [
   },
 ];
 
-console.log(data[0]);
-console.log(data[0].name);
-console.log(data[0].test);
-console.log(data[0].test.test.a);
-console.log(data[1]);
-console.log(data[1].haha.test.test.a);
-
-const types = Object.keys(data);
-console.log(types);
-const entries = Object.entries(data);
-console.log(entries);
-
 //giveMeSum(data) // sum of all elements of haha array
 
-function getProperty(data, test) {
-  for (var i = 0; i < test.length; i++) {
-    data = data[test[i]];
-    if (!data) {
-      break;
+function giveMeSum(search, id) {
+  for (var i = 0; i <= search.length; i++) {
+    if (i == id) {
+      Object.keys(search[i]).forEach((key) => {
+        if (key == "test" || key == "haha") {
+          Object.keys(search[i][key]).forEach((item) => {
+            if (Object.keys(search[i][key][item]) == "a") {
+              for (var j = 0; j <= search[i][key][item].a.length - 1; j++) {
+                Object.keys(search[i][key][item].a[j]).forEach((el) => {
+                  if (typeof search[i][key][item].a[j][el] != "boolean") {
+                    let sum = 0;
+                    Object.keys(search[i][key][item].a[j][el]).forEach(
+                      function (elem) {
+                        sum += Number(elem);
+                      }
+                    );
+                    console.log(sum);
+                  }
+                });
+              }
+            }
+            if (Object.keys(search[i][key][item]) == "test") {
+              Object.keys(search[i][key][item]).forEach((element) => {
+                if (Object.keys(search[i][key][item][element]) == "a") {
+                  for (
+                    var j = 0;
+                    j <= search[i][key][item][element].a.length - 1;
+                    j++
+                  ) {
+                    Object.keys(search[i][key][item][element].a[j]).forEach(
+                      (el) => {
+                        if (
+                          typeof search[i][key][item][element].a[j][el] !=
+                          "boolean"
+                        ) {
+                          let sum = 0;
+                          Object.keys(
+                            search[i][key][item][element].a[j][el]
+                          ).forEach(function (elem) {
+                            sum += Number(elem);
+                          });
+                          console.log(sum);
+                        }
+                      }
+                    );
+                  }
+                }
+              });
+            }
+          });
+        }
+      });
     }
-    return data;
   }
 }
 
-console.log(getProperty(data, ["haha", "test"]));
-
-function giveMeSum(elements) {
-  for (var i = 0; i <= elements.length; i++) {
-    for (var j = 0; j <= elements[i].length; j++) {
-      console.log(elements[j]);
-    }
-  }
-}
-
-let pjrabotay = giveMeSum(data);
-console.log(pjrabotay);
-console.log(data);
+giveMeSum(data, 1);
+giveMeSum(data, 0);
 
 //findValue(data, 2) // false or true
 
-function findValue(element, index) {
-  for (var i = 0; i <= element.length; i++) {
-    if (i == index) {
-      for (var j = 0; j <= data[i].length; j++) {
-        console.log(data[j]);
-      }
+function findValue(search, id) {
+  for (var i = 0; i <= search.length; i++) {
+    if (i == id) {
+      Object.keys(search[i]).forEach((key) => {
+        if (key == "test" || key == "haha") {
+          Object.keys(search[i][key]).forEach((item) => {
+            if (Object.keys(search[i][key][item]) == "a") {
+              for (var j = 0; j <= search[i][key][item].a.length - 1; j++) {
+                Object.keys(search[i][key][item].a[j]).forEach((el) => {
+                  if (el == "haha") {
+                    console.log(search[i][key][item].a[j][el]);
+                  }
+                });
+              }
+            }
+            if (Object.keys(search[i][key][item]) == "test") {
+              Object.keys(search[i][key][item]).forEach((element) => {
+                if (Object.keys(search[i][key][item][element]) == "a") {
+                  for (
+                    var j = 0;
+                    j <= search[i][key][item][element].a.length - 1;
+                    j++
+                  ) {
+                    Object.keys(search[i][key][item][element].a).forEach(
+                      (el) => {
+                        if (el == "haha") {
+                          console.log(search[i][key][item][element].a[j][el]);
+                        }
+                      }
+                    );
+                  }
+                }
+              });
+            }
+          });
+        }
+      });
     }
   }
 }
 
-let something = findValue(data, 0);
-console.log(data);
+findValue(data, 1);
+findValue(data, 0);
